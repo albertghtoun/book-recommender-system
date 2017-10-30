9e39a148a985e1f96499683fb104a8096f1ab3be
Chao Wang <albertwang87@gmail.com>
2017-10-30 08:52:11 +0000

    updated dataset download link

--- cmds.sh
+++ cmds.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo yum install git
 sudo pip install -r requirements.txt
 ./download_dataset.sh
 #hdfs dfs -put datasets/ hdfs://ip-172-31-13-184.us-west-1.compute.internal:8020/user/hadoop/
--- download_dataset.sh
+++ download_dataset.sh
@@ -3,8 +3,5 @@
 hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
 hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
 
-wget http://files.grouplens.org/datasets/movielens/ml-latest.zip
-unzip -o "ml-latest.zip"
-DESTINATION="./datasets/"
-mkdir -p $DESTINATION
-mv ml-latest $DESTINATION
+wget http://alchem.usc.edu/~chao/download/book_datasets.zip
+unzip -o "book_datasets.zip"
