6902f16a12c3bdab92e41c4315028bc5b9ebc266
Chao Wang <albertwang87@gmail.com>
2017-11-20 11:17:21 -0800

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from pyspark import SparkContext, SparkConf
 
 def init_spark_context():
     # load spark context
-    conf = SparkConf().setAppName("movie_recommendation-server")
+    conf = SparkConf().setAppName("book_recommendation-server")
     # IMPORTANT: pass aditional Python modules to each worker
     sc = SparkContext(conf=conf, pyFiles=['engine.py', 'app.py'])
  
